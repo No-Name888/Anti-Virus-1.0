2399fcb7ccabdd060a5541c9628a92bb07917f84
No-Name888 <lucaswang889@gmail.com>
2025-08-02 21:27:19 -0700

    Update HowToUse!.py

--- HowToUse!.py
+++ HowToUse!.py
@@ -1,2 +1,2 @@
 # thanks for checking this out. Down load the main.py once your done reading the README.md. Put main.py in a coding software that supports [Example].py
-# also [wip]
+# also you will have to wait a black screen for A WHILE but once done it could do 1000 files in 1 sec. May not be the MOST accurate but still makes a good let-knower
